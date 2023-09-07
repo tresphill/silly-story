51f72dab7b3dea0c16ee42d2e50e466d50fd4daa
tres phillips <tphill760@gmail.com>
2023-09-07 19:29:57 +0000

    index.html

--- main.js
+++ main.js
@@ -2,52 +2,51 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array) {
-    const random = Math.floor(Math.random() * array.length);
-    return array[random];
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
 }
 
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
-let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
-let insertX = "Willy the Goblin, Big Daddy, Father Christmas"
-let insertY = "the soup kitchen, Disneyland, the White House"
-let insertZ = "spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away"
-
-randomize.addEventListener('click', result);
-
-function result() {
-    if (customName.value !== '') {
-        let name = "custom.value";
-    }
-}
-if document.getElementById("uk").checked) {
-    let weight = Math.round(300);
-    let temperature = Math.round(94);
-}
-story.text = '';
-story.computedStyleMap.visibility = 'visible';
 
 let newStory = storyText;
+let xItem = randomValueFromArray(insertX);
+let yItem = randomValueFromArray(insertY);
+let zItem = randomValueFromArray(insertZ);
+randomize.addEventListener('click', result);
 
-let xItem = randomValueFromArray;
-let yItem = randomValueFromArray;
-let zItem = randomValueFromArray;
+newStory = newStory.replaceAll(':insertx:', xItem);
+    newStory = newStory.replaceAll(':inserty:', yItem);
+    newStory = newStory.replaceAll(':insertz:', zItem);
+    newStory = newStory.replaceAll(':insertx:', xItem);
+    
+function result() {
+    let newStory = storyText;
 
-function newStory("xItem", "yItem", "zItem"); {
-    newStory.contentString.replace(insertX, insertY, insertZ);
-    content.contentString.replace("xItem", "yItem", "zItem");
-}
-
-var input = document.getElementById("customName");
-if input !== input.value {
-    customName = "Bob";
-}
-
-if document.getElementById("uk").checked {
-    weight = Math.round(300 / 14) + 'stone';
-    temperature = Math.round(temperature - 32 x .5556); + 'centigrade';
-    temperature.replace('94 Farenheit', 'temperature');
-    weight.replace('300 pounds', 'weight');
-
-    textContent.story('p') = newStory;
+    if (customName.value !== "") {
+      let name = customName.value;
+      newStory = newStory.replaceAll("Bob", name);
+    }
+    if (document.getElementById("uk").checked) {
+      let weight = Math.round(300 / 14) + " stone";
+      let temperature = Math.round(((94 - 32) * 5) / 9) + " centigrade";
+      newStory = newStory.replaceAll("94 fahrenheit", temperature);
+      newStory = newStory.replaceAll("300 pounds", weight);
+    }
+  
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
+  
+    newStory = newStory.replaceAll(":insertx:", xItem);
+    newStory = newStory.replaceAll(":insertx:", xItem);
+    newStory = newStory.replaceAll(":inserty:", yItem);
+    newStory = newStory.replaceAll(":insertz:", zItem);
+  
+    story.textContent = newStory;
+    story.style.visibility = "visible";
 }
