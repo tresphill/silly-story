de3e14a9ee5a437afed9e8af4c195a215eae3cc5
tres phillips <tphill760@gmail.com>
2023-09-07 20:16:35 +0000

    index.html

--- main.js
+++ main.js
@@ -6,19 +6,20 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
-
+//this is the body where the story is written
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
-let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+let insertX = ['Barbara Streisand', 'Papa John', 'that guy from the My Pillow commercial'];
 let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
-
+//i need it to randomly change the story when the button is pressed
 let newStory = storyText;
 let xItem = randomValueFromArray(insertX);
 let yItem = randomValueFromArray(insertY);
 let zItem = randomValueFromArray(insertZ);
 randomize.addEventListener('click', result);
 
+//the new story created each time will pull from this
 newStory = newStory.replaceAll(':insertx:', xItem);
     newStory = newStory.replaceAll(':inserty:', yItem);
     newStory = newStory.replaceAll(':insertz:', zItem);
